c4dee24b0faff565069cbf19d18e2ad503a865d5
Eman Omar <40348594+eomar001@users.noreply.github.com>
2019-03-15 09:46:15 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car implements Vehicle{
+    private VehicleType type;
+    public Car (VehicleType type){
+        this.type= type;
+    }
+   public VehicleSize getSize(){
+        return VehicleSize.SMALL;
+   }
+   public VehicleType getType(){
+        return type;
+   }
+}
+
