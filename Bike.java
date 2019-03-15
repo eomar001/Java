e4bf2f85ba3221f430c4733dfbfbfda78d37eb5a
Eman Omar <40348594+eomar001@users.noreply.github.com>
2019-03-15 09:43:40 -0500

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,12 @@
+public class Bike  implements Vehicle{
+    private VehicleType type;
+    public Bike (VehicleType type){
+        this.type= type;
+    }
+   public VehicleSize getSize(){
+        return VehicleSize.SMALL;
+   }
+   public VehicleType getType(){
+        return type;
+   }
+}
