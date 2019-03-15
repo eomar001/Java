6bfa00eed06cde31b6df92f0324cd8427cb2ac60
Eman Omar <40348594+eomar001@users.noreply.github.com>
2019-03-15 09:42:36 -0500

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+public interface Vehicle {
+    enum VehicleSize {
+        SMALL;
+        MEDIUM;
+        LARGE;
+    }
+    enum VehicleType {
+        REGULAR;
+        HANDICAPPED;
+    }
+    VehicleSize getSize();
+    VehicleType getType();
+    }
